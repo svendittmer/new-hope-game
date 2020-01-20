e90dd6d1787da7d3673a607c53ece58ba8991c45
Sven <sven.dittmer@gmx.net>
2020-01-20 22:11:45 +0100

    Add a scene

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { Engine, Scene, ArcRotateCamera, HemisphericLight, Vector3, MeshBuilder, Mesh } from "babylonjs";
+import { addLabelToMesh } from "./gui";
 
 var canvas: any = document.getElementById("renderCanvas");
 var engine: Engine = new Engine(canvas, true);
@@ -8,6 +9,12 @@ function createScene(): Scene {
 
     var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
+
+    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+
+    var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+
+    addLabelToMesh(sphere);
 
     return scene;
 }
